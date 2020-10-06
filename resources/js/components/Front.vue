071c51cfe233432800aa9c420195e10eed3ccc5f
HARON <haronur@gmail.com>
2020-10-06 15:22:21 +0600

    Laravel-Vue-Sidebar-Multi-Filters 01

---
+++ resources/js/components/Front.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container" >
+        <div class="row">
+            <div class="col-lg-3 mb-4">
+                <h1 class="mt-4">Filters</h1>
+
+                <h3 class="mt-2">Price</h3>
+                <div class="form-check" >
+                    <input class="form-check-input" type="checkbox">
+                    <label class="form-check-label">
+                        100 200
+                    </label>
+                </div>
+
+                <h3 class="mt-2">Categories</h3>
+                <div class="form-check" >
+                    <input class="form-check-input" type="checkbox">
+                    <label class="form-check-label" >
+                        noha
+                    </label>
+                </div>
+
+                <h3 class="mt-2">Manufacturers</h3>
+                <div class="form-check" >
+                    <input class="form-check-input" type="checkbox" >
+                    <label class="form-check-label" >
+                        Hunda
+                    </label>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="row mt-4">
+                    <div class="col-lg-4 col-md-6 mb-4" >
+                        <div class="card h-100">
+                            <a href="#">
+                                <img class="card-img-top" src="http://placehold.it/700x400" alt="">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="#">Lapto</a>
+                                </h4>
+                                <h5>34000</h5>
+                                <p class="card-text">It is a Good Product</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Shop Catalog</title>
-<!--     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/shop-homepage.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/loading.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/loading.css') }}" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
@@ -20,7 +20,7 @@
 </nav>
 
 <div id="app">
-    <example-component></example-component>
+    <front-page></front-page>
 </div>
 
 <footer class="footer py-5 bg-dark">
